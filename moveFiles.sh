878ad4c81a06094f5ef618413c4109cab4302097
Fethallah Benmansour <fethallah@gmail.com>
2011-09-22 14:49:46 +0200

    Remove the moveFiles.sh script

--- moveFiles.sh
+++
@@ -1,3 +0,0 @@
-cp /home/fbenmans/Downloads/src_tubularity_FIJI/plugins/MultiScaleTubularityMeasure_Plugin.jar /home/fbenmans/Downloads/Fiji.app/plugins/.
-
-cp /home/fbenmans/Downloads/src_tubularity_FIJI/build/linux64/*.so /home/fbenmans/Downloads/Fiji.app/plugins/ITK/linux64/.
