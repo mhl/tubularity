df162d262c10fc8aff9c5bb062f86a78d3faeda1
Mark Longair <mhl@pobox.com>
2011-09-23 10:35:35 +0200

    Added skeleton of the C++ implementation of getPathResult
    This compiles, but is so far untested...

--- c++/MultiScaleTubularityMeasureJNIImplementation.cpp
+++ c++/MultiScaleTubularityMeasureJNIImplementation.cpp
@@ -43,6 +43,7 @@ SwitchCase(OrientedFluxCrossSectionCurvature, OrientedFluxMainCurvatureMultiScal
 #define GRAY32 4
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
 const unsigned int maxDimension = 3;
@@ -497,16 +498,178 @@ JNIEXPORT jint JNICALL Java_FijiITKInterface_MultiScaleTubularityMeasure_Oriente
     return 0;
 }
 
+void setSuccess(JNIEnv * env,
+                bool succeeded,
+                jobject pathResultObject,
+                jclass pathResultClass)
+{
+    jmethodID mid = env->GetMethodID(pathResultClass,
+                                     "setSuccess",
+                                     "(Z)V");
+    if (!mid) {
+        std::cout << "Failed to find the setSuccess method";
+        return;
+    }
+
+    jboolean jSucceeded = succeeded ? 1 : 0;
+
+    env->CallVoidMethod(pathResultObject,
+                        mid,
+                        jSucceeded);
+}
+
+void setErrorMessage(JNIEnv * env,
+                     const char * message,
+                     jobject pathResultObject,
+                     jclass pathResultClass)
+{
+    jmethodID mid = env->GetMethodID(pathResultClass,
+                                     "setErrorMessage",
+                                     "(Ljava/lang/String;)V");
+    if (!mid) {
+        std::cout << "Failed to find the setErrorMessage method";
+        return;
+    }
+
+    jstring jMessage = env->NewStringUTF(message);
+
+    env->CallVoidMethod(pathResultObject,
+                        mid,
+                        jMessage);
+
+    env->DeleteLocalRef(jMessage);
+
+    setSuccess(env,
+               false,
+               pathResultObject,
+               pathResultClass);
+}
+
 /*
  * Class:     FijiITKInterface_MultiScaleTubularityMeasure
  * Method:    getPathResult
  * Signature: (Ljava/lang/String;[F[FLFijiITKInterface/PathResult;)V
  */
 JNIEXPORT void JNICALL Java_FijiITKInterface_MultiScaleTubularityMeasure_getPathResult
- (JNIEnv *, jobject, jstring, jfloatArray, jfloatArray, jobject) {
+ (JNIEnv * env,
+  jobject ignored,
+  jstring jTubularityFilename,
+  jfloatArray jPoint1,
+  jfloatArray jPoint2,
+  jobject pathResultObject)
+{
 
-    // TODO: fill in the code here
+    jclass pathResultClass = env->GetObjectClass(pathResultObject);
 
+    // Check that the float arrays are of the right length:
 
+    int pt1Length = env->GetArrayLength(jPoint1);
+    int pt2Length = env->GetArrayLength(jPoint2);
 
+    if (pt1Length != 3) {
+        setErrorMessage(env,
+                        "pt1 was not of length 3",
+                        pathResultObject,
+                        pathResultClass);
+        return;
+    }
+
+    if (pt2Length != 3) {
+        setErrorMessage(env,
+                        "pt2 was not of length 3",
+                        pathResultObject,
+                        pathResultClass);
+        return;
+    }
+
+    // And convert them to C types:
+
+    jboolean isPoint1Copy, isPoint2Copy;
+    jfloat * pt1 = env->GetFloatArrayElements(jPoint1, &isPoint1Copy);
+    jfloat * pt2 = env->GetFloatArrayElements(jPoint2, &isPoint2Copy);
+
+    // Some debugging, just print out the start and end points:
+
+    for(int i = 0; i < 3; ++i ) {
+        std::cout << "pt1[" << i << "] is " << pt1[i] << std::endl;
+    }
+    for(int i = 0; i < 3; ++i ) {
+        std::cout << "pt2[" << i << "] is " << pt2[i] << std::endl;
+    }
+
+    // Now get the filename:
+
+    const char * filename = env->GetStringUTFChars(jTubularityFilename, NULL);
+    if (!filename) {
+        setErrorMessage(env,
+                        "Failed to convert the filename",
+                        pathResultObject,
+                        pathResultClass);
+        env->ReleaseFloatArrayElements(jPoint1, pt1, 0);
+        env->ReleaseFloatArrayElements(jPoint2, pt2, 0);
+        return;
+    }
+
+    // ------------------------------------------------------------------------
+    // FIXME: real code should go here, just example data:
+
+    int points = 40;
+    float * pathPoints = new float[points*4];
+
+    for(int i = 0; i < points; ++i ) {
+        pathPoints[i] = i;
+        pathPoints[i+1] = i;
+        pathPoints[i+2] = i;
+        pathPoints[i+3] = 6 + ((i % 8) - 4);
+    }
+
+    // Now convert that to a Java float array:
+
+    jfloatArray jResultArray = env->NewFloatArray(points * 4);
+    if (!jResultArray) {
+        std::cout << "Failed to allocate a new Java float array";
+        env->ReleaseFloatArrayElements(jPoint1, pt1, 0);
+        env->ReleaseFloatArrayElements(jPoint2, pt2, 0);
+        env->ReleaseStringUTFChars(jTubularityFilename, filename);
+        return;
+    }
+
+    env->SetFloatArrayRegion(jResultArray,
+                             0,
+                             points * 4,
+                             (jfloat *)pathPoints);
+
+    {
+        jmethodID mid = env->GetMethodID(pathResultClass,
+                                         "setPath",
+                                         "([F)V");
+        if (!mid) {
+            std::cout << "Failed to find the setPath method";
+            env->ReleaseFloatArrayElements(jPoint1, pt1, 0);
+            env->ReleaseFloatArrayElements(jPoint2, pt2, 0);
+            env->ReleaseStringUTFChars(jTubularityFilename, filename);
+            return;
+        }
+
+        env->CallVoidMethod(pathResultObject,
+                            mid,
+                            jResultArray);
+    }
+
+    env->DeleteLocalRef(jResultArray);
+
+    delete [] pathPoints;
+
+    // ------------------------------------------------------------------------
+
+    setSuccess(env,
+               true,
+               pathResultObject,
+               pathResultClass);
+
+    /* Release anything allocated... */
+
+    env->ReleaseFloatArrayElements(jPoint1, pt1, 0);
+    env->ReleaseFloatArrayElements(jPoint2, pt2, 0);
+    env->ReleaseStringUTFChars(jTubularityFilename, filename);
 }
